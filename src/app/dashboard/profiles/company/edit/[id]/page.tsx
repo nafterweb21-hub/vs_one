10ce9624281781ca64cc0142a55ccb8b62dacaa3
divyashree <divyashree@nafter.in>
2026-05-23 10:28:23 +0530

    fix: resolve import error in company profile edit page

--- src/app/dashboard/profiles/company/edit/[id]/page.tsx
+++ src/app/dashboard/profiles/company/edit/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
-import CompanyProfileForm from "../components/CompanyProfileForm";
+import CompanyProfileForm from "../../components/CompanyProfileForm";
 
 export default function EditCompanyProfilePage() {
   const params = useParams();
